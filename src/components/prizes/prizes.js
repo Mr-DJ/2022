d641b43793a07cc7cb83c943e65e863a17f14ef4
charana555 <charanchandrashekar55@gamil.com>
2022-09-25 21:41:29 +0530

    Added Prizes Section

--- src/components/prizes/prizes.js
+++ src/components/prizes/prizes.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
+
+import bg from "../../images/prizes_bg.png";
 
 import "./prize.css";
 
-
 const Prizes = () => {
   return (
-    <div id="prizes" className='about-main d-flex align-items-center justify-content-center'>
+    <div id="prizes" className="prizes-main">
       <Container>
-        <div data-aos="slide-up" className='about-heading'>
-          <h1 className='heading'>Prizes</h1>
-          <img className='line' src='/about_bg.png' width={"100%"} alt='' />
+        <div data-aos="slide-up" className="prize-heading-div">
+          <h1 className="prize-heading">PRIZES</h1>
+          <img className="prize-img" src={bg} width={"100%"} alt="" />
         </div>
-        <Container>
-          {/* Main Prizes Content */}
+      </Container>
+      <Container className="prize-container">
+        <Container className="prize-1">
+          <div className="prize">
+            <h5 className="prize-title">1st Prize</h5>
+            <p className="prize-amt"> ₹ xx,xxx </p>
+          </div>
+        </Container>
+        <Container className="prizes">
+          <div className="prize">
+            <h5 className="prize-title">2nd Prize</h5>
+            <p className="prize-amt"> ₹ xx,xxx </p>
+          </div>
+          <div id="prize3" className="prize">
+            <h5 className="prize-title">3nd Prize</h5>
+            <p className="prize-amt"> ₹ xx,xxx </p>
+          </div>
         </Container>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Prizes
-
-
+export default Prizes;
