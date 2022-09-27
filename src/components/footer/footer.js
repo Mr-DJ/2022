b1545fff748a3a6a6c56f4453c2d3acb0516e2ac
Mohit20Kumar <mohitkumar20021220@gmail.com>
2022-09-28 00:27:01 +0530

    footer done again

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,82 +1,63 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import {
+  SiGmail,
+  SiLinkedin,
+  SiInstagram,
+  SiGithub,
+  SiDiscord,
+} from "react-icons/si";
 import "./footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
-      <div class="site-section">
-        <Container>
-          <div class="d-flex px-5 justify-content-between align-items-center">
-            <div class="col-sm">
-              <ul class="list-unstyled links">
-                <li>
-                  <a href="#about" class="footer-link">
-                    ABOUT
-                  </a>
-                </li>
-                <li>
-                  <a href="#tracks" class="footer-link">
-                    TRACKS
-                  </a>
-                </li>
-                <li>
-                  <a href="#prizes" class="footer-link">
-                    PRIZES
-                  </a>
-                </li>
-                <li>
-                  <a href="#sponsors" class="footer-link">
-                    SPONSORS
-                  </a>
-                </li>
-                <li>
-                  <a href="#faq" class="footer-link">
-                    FAQ
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-md-3 contact">
-              <h1>CONTACT US</h1>
-              <ul class="list-unstyled contact-items">
-                {/* <li><h2>+91 8127982371</h2></li> */}
-                <li>
-                  <h2>
-                    <a
-                      href="mailto:info.revahack@gmail.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      info.revahack@gmail.com
-                    </a>
-                  </h2>
-                </li>
-                <li>
-                  <h2>
-                    <a
-                      href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Code of Conduct
-                    </a>
-                  </h2>
-                </li>
+    <div className='footer'>
+      <Container>
+        <div className='px-5 footer-icons'>
+          <a
+            href='mailto:info.revahack@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <SiGmail />
+          </a>
+          <a
+            href='https://www.linkedin.com/company/reva-hack-2022/'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <SiLinkedin />
+          </a>
+          <a
+            href='https://discord.gg/6Sv9ZdQ4er'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <SiDiscord />
+          </a>
+          <a
+            href='https://instagram.com/dscreva?igshid=YmMyMTA2M2Y='
+            target='_blank'
+            rel='noopener noreferrer'>
+            <SiInstagram />
+          </a>
+          
+          <a
+            href='https://github.com/revahack'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <SiGithub />
+          </a>
+        </div>
 
-                {/* <li><h2>info.revahack@gmail.com</h2></li> */}
-                {/* <li><h2>contact@reva.com</h2></li> */}
-              </ul>
-            </div>
-          </div>
-          <div className="copy">
-            <p class="copyright">
-              <h6>Copyright REVA Hack &#169;2022</h6>
-              {/* <h6 style={{ margin:"0" }}>Made with &#x2665; by Tech Team</h6> */}
-            </p>
-          </div>
-        </Container>
+      <div className="more-links">
+
+        <a
+          href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf'
+          target='_blank'
+          rel='noopener noreferrer'>
+          CODE OF CONDUCT
+        </a>
+        <span>Copyright 2022 REVA HACK</span>
       </div>
+      </Container>
     </div>
   );
 }
